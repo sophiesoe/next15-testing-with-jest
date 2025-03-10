081573934997c52de15d07b4313c08c63402461d
sophiesoe <devsophiesoe@gmail.com>
2025-03-10 16:47:31 +0630

    add: sample todo app

---
+++ app/src/pages/home/Home.page.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Home from "./Home.page";
+
+describe("TodoComponent", () => {
+  //  before each test, render TodoApp
+  beforeEach(() => {
+    render(<Home />);
+  });
+
+  test("render the title", async () => {
+    const title = screen.getByTestId("title");
+    expect(title).toBeInTheDocument();
+  });
+});
